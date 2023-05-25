5e27c3db0ce9e4607a9f990a6a0f1a7f67b1b49a
lsyyy310 <lsyyy310@gmail.com>
2023-05-25 23:53:59 +0800

    delete tkinter.ttk

--- Page01~04.py
+++ Page01~04.py
@@ -2,7 +2,6 @@
 import tkinter as tk
 import tkinter.font as tkFont
 import tkmacosx as tkmac
-from tkinter import ttk
 from PIL import Image, ImageTk
 from itertools import count, cycle
 from module_trust import Trust
